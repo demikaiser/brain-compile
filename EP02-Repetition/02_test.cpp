c83db161db8a500dd83ef01392831a5ef6f8d1e5
Jake <demikaiser13@gmail.com>
2023-08-01 09:35:51 +0900

    update

---
+++ EP02-Repetition/02_test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int addNumbers(int a, int b) {
+    return a + b;
+}
+
+int main() {
+
+
+    cout << 2 << endl;
+
+    cout << "Hello World" << endl;
+
+    int number = addNumbers(1, 2);
+
+    cout << number << endl;
+
+    return 0;
+}
+
+
